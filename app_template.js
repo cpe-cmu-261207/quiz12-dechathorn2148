f60d876b0ddddd259ee59b9fd4311c30d6d6a15c
Dechathorn <59277863+dechathorn2148@users.noreply.github.com>
2021-01-12 14:02:15 +0700

    Update app_template.js

--- app_template.js
+++ app_template.js
@@ -1,1 +1,27 @@
 //your code here!
+let idstudent = 620612148;
+let row;
+let col;
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#button-compute').disable = true;
+    
+
+    document.querySelector('#button-compute').onclick = function(){
+        let num = document.querySelector('#input-number').value;
+        idstudent = idstudent + parseInt(num);
+        document.querySelector('#idstudent').innerHTML = idstudent;
+    }
+
+    document.querySelector('#button-create-table').onclick = function(){
+        let row = document.querySelector('#input-row').value;
+        let col = document.querySelector('#input-col').value;
+
+        var tbl = document.createElement("table");
+        var tblBody = document.createElement("tbody");
+
+        
+    }
+    
+});
